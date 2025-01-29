3a973c8c0ee717de219f2707cfb4fc9a17d434ad
Lukas Kubanek <lukas.kubanek@me.com>
2025-01-29 14:58:51 +0200

    Tweaked docs comment

--- Sources/CombineExtensions/CurrentValuePublisher/CurrentValuePublisher+KVO.swift
+++ Sources/CombineExtensions/CurrentValuePublisher/CurrentValuePublisher+KVO.swift
@@ -9,10 +9,12 @@ extension KVOCurrentValuePublishing where Self: NSObject {
     /// Returns a `CurrentValuePublisher` that tracks the current value of a KVO-compliant property.
     ///
     /// - Parameter keyPath: The key path of the property to observe.
-    /// - Returns: A `CurrentValuePublisher` that tracks the property's value.
+    /// - Returns: A `CurrentValuePublisher` that tracks the property’s value.
     ///
-    /// This implementation follows the approach of `NSObject.publisher(for:options:)` from Foundation,
-    /// previously available in the [swift-corelibs-foundation](https://bit.ly/nsobject-keyvalueobserving)
+    /// This implementation follows the approach of `NSObject.publisher(for:options:)` from
+    /// Foundation. Its exact up-to-date declaration is difficult to find in Apple’s documentation,
+    /// as discussed in [this Stack Overflow post](https://stackoverflow.com/q/60381905/670119),
+    /// but it can be found in an older version of the [swift-corelibs-foundation](https://bit.ly/nsobject-keyvalueobserving)
     /// open source repository.
     public func currentValuePublisher<Value>(
         for keyPath: KeyPath<Self, Value>
